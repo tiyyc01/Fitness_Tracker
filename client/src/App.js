c0d68008e803036f00e5234abde0403c23af3525
Chinmaie Tiyyagura <tiyyc01@pfw.edu>
2024-06-20 11:30:41 -0500

    User authentication

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { ThemeProvider, styled } from 'styled-components';
+import { lightTheme } from './utils/Themes';
+import { BrowserRouter } from 'react-router-dom'
+import Authentication from './pages/Authentication';
+
+const Container = styled.div`
+width:100%;
+heigth:100vh;
+display:flex;
+flex-direction: column;
+background: ${({ theme }) => theme.bg};
+color:${({ theme }) => theme.text_primary};
+overflow-x:hidden;
+overflow-y:hidden;
+transition: all 0.2s easy;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={lightTheme}>
+      <BrowserRouter>
+        <Container>
+          <Authentication />
+        </Container>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
